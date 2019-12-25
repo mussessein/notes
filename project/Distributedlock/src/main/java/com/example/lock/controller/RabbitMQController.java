6ad80b156ded1359c7fa7bcceb027e599151473d
mussessein <2482600250@qq.com>
2019-12-25 21:25:05 +0800

    project

--- project/Distributedlock/src/main/java/com/example/lock/controller/RabbitMQController.java
+++ project/Distributedlock/src/main/java/com/example/lock/controller/RabbitMQController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.example.lock.controller;
 
 import com.example.lock.dto.RobbingDto;
@@ -48,54 +47,3 @@ public class RabbitMQController {
         return response;
     }
 }
-=======
-package com.example.lock.controller;
-
-import com.example.lock.dto.RobbingDto;
-import com.example.lock.entity.User;
-import com.example.lock.rabbitmq.RabbitMQService;
-import com.example.lock.response.BaseResponse;
-import com.example.lock.response.StatusCode;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@Slf4j
-@RequestMapping("/rabbitmq")
-public class RabbitMQController {
-
-    @Autowired
-    private RabbitMQService rabbitMQService;
-
-    @PostMapping(value = "/sendBasicMsg")
-    public BaseResponse sendMessage(@RequestParam String msg) {
-        BaseResponse response = new BaseResponse(StatusCode.Success);
-        try {
-            rabbitMQService.sendMessage(msg);
-            log.info("Send Message Success => {}",msg);
-        } catch (Exception e) {
-            e.printStackTrace();
-            response =new BaseResponse(StatusCode.Fail);
-        }
-        return response;
-    }
-    /**
-     * crm系统抢单请求
-     * @return
-     */
-    @PostMapping(value = "/sendCrmMsg")
-    public BaseResponse crmOrderInfo(@RequestBody RobbingDto dto){
-        BaseResponse response=new BaseResponse(StatusCode.Success);
-        try {
-            rabbitMQService.sendCrmOrderInfo(dto);
-        }catch (Exception e){
-            e.printStackTrace();
-            response=new BaseResponse(StatusCode.Fail);
-        }
-        return response;
-    }
-}
->>>>>>> project
